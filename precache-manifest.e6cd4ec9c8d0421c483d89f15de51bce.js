4ff0cad9f9c9e2a5abe321dbbf452256cc90069b
Annamalai Muthalagappan <m.annamalai87@gmail.com>
2020-03-24 13:44:43 -0500

    Updates

---
+++ precache-manifest.e6cd4ec9c8d0421c483d89f15de51bce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879c76f2e8b6f8c7dace84d25d1cddac",
+    "url": "/GroceryList/index.html"
+  },
+  {
+    "revision": "98fe131259df1212b846",
+    "url": "/GroceryList/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "dfad548d53951a0b4a3c",
+    "url": "/GroceryList/static/css/main.ea3baea5.chunk.css"
+  },
+  {
+    "revision": "98fe131259df1212b846",
+    "url": "/GroceryList/static/js/2.995cc0b0.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/GroceryList/static/js/2.995cc0b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfad548d53951a0b4a3c",
+    "url": "/GroceryList/static/js/main.2a0a8c3f.chunk.js"
+  },
+  {
+    "revision": "c57beb46459f0ba49bb5",
+    "url": "/GroceryList/static/js/runtime-main.3cd584de.js"
+  }
+]);
